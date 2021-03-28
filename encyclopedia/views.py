2348f7cb4b41e3cb7a63b52c62999099d4c6e1ec
Kenneth Weaver <kaw0829@gmail.com>
2021-03-27 22:20:41 -0700

    added hyperlinks and articles

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
@@ -6,5 +7,13 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
-    })
+        })
 
+def get_entry(request, title):
+    # add some checking for get entry in case it returns none
+    entry = util.get_entry(title)
+    entry = markdown2.markdown(entry)
+    return render(request, "encyclopedia/entry_page.html", {
+        "entry_title": title,
+        "entry_info": entry
+    })
